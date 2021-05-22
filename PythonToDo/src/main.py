6b5cca259c34e0cbd1dd1f58270c2675bf01d2e7
eddiejosue14 <43359575+eddiejosue14@users.noreply.github.com>
2021-05-22 00:18:52 -0600

    Python ToDo Flask
    Python ToDo Flask

---
+++ PythonToDo/src/main.py
@@ -0,0 +1,81 @@
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from admin import setup_admin
+from models import db, User, ToDo
+#from models import Person
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+MIGRATE = Migrate(app, db)
+db.init_app(app)
+CORS(app)
+setup_admin(app)
+
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
+
+@app.route('/user', methods=['GET'])
+def handle_hello():
+
+    response_body = 
+    {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    result = ToDo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), result))
+    return jsonify(all_todos), 200
+
+@app.route('/todos', methods=['POST'])
+def post_todos():
+
+    request_body = request.get_json()
+    todo = ToDo(label=request_body["label"],done=request_body["done"])
+    db.session.add(todo)
+    db.session.commit()
+
+    result = ToDo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), result))
+
+    return jsonify(all_todos), 200
+
+
+@app.route('/todos/<int:tid>', methods=['DELETE'])
+def delete_todo(tid):
+
+    todo = ToDo.query.get(tid)
+
+    if todo is None:
+        raise APIException('ToDo not found', status_code=404)
+    db.session.delete(todo)
+    db.session.commit()
+
+    result = ToDo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), result))
+
+    return jsonify(all_todos), 200    
+
+# this only runs if `$ python src/main.py` is executed
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
---
+++ PythonToDo/src/models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(200),nullable=False)
+    done = db.Column(db.Boolean(),nullable=False)
+
+    def __repr__(self):
+        return '<ToDo %r' % self.label
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "label":self.label,
+            "done":self.done
+        }
